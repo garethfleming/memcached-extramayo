c332d7a617fca71516b68bdfcb0dfd82b3de392b
Gareth Fleming <Gareth@numpty.local>
2010-04-21 14:59:40 +0100

    Just getting the code to build until I decide how the xmpp stuff should actually work

--- xmpp.c
+++ xmpp.c
@@ -1,1 +1,9 @@
 #include "xmpp.h"
+
+int start_xmpp(const char *jid) {
+    return 0;
+}
+
+int stop_xmpp(void) {
+    return 0;
+}
--- xmpp.h
+++ xmpp.h
@@ -1,4 +1,7 @@
 #ifndef _XMPP_H
 #define _XMPP_H
 
+int start_xmpp(const char *jid);
+int stop_xmpp(void);
+
 #endif
